f4fe253484d702c86598c3ede1f0665cffaeddd5
Isai-gomez <isaigomezhernandez@gmail.com>
2020-05-04 15:46:47 -0500

    Se agrega la Lista de item al todo-list

--- app.js
+++ app.js
@@ -8,6 +8,27 @@ todoButton.addEventListener("click", addTodo);
 
 //Fsunctions
 function addTodo(event) {
+  //Prevent form from submitting
   event.preventDefault();
-  console.log("hola");
+  //Todo DIV
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "HELLO";
+  newTodo.classList.add("todo-item");
+  //Adding in todoDiv
+  todoDiv.appendChild(newTodo);
+  //Check mark  button
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"><i>';
+  completedButton.classList.add("complete-btn");
+  todoDiv.appendChild(completedButton);
+  //Check trash button
+  const completedTrash = document.createElement("button");
+  completedTrash.innerHTML = '<i class="fas fa-trash"></i>';
+  completedTrash.classList.add("complete-btn");
+  todoDiv.appendChild(completedTrash);
+  //Apend to list
+  todoList.appendChild(todoDiv);
 }
