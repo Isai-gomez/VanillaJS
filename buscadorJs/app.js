7f2dcd43c581d4d8e838f88a0537303a373debe5
Isai-gomez <isaigomezhernandez@gmail.com>
2020-05-17 18:22:39 -0500

    puedo ver el tecto por consola

--- buscadorJs/app.js
+++ buscadorJs/app.js
@@ -1,4 +1,16 @@
 import { frutas } from "./data/data.js";
-export const buscador = () => {
+
+//Varibles globales
+// const formularioUI = documnet.querySelector(".formulario");
+const textUI = document.querySelector("#textoIU");
+const botonUI = document.querySelector("#botonUI");
+const resultUI = document.querySelector("#botonUI");
+
+//Eventos
+botonUI.addEventListener("click", buscar);
+
+//Funciones
+export function buscar(event) {
+  event.preventDefault();
   console.log(frutas);
-};
+}
--- buscadorJs/data/data.js
+++ buscadorJs/data/data.js
@@ -2,4 +2,5 @@ export const frutas = [
   { nombre: "Mango", precio: 30 },
   { nombre: "Manzana", precio: 24 },
   { nombre: "Durazno", precio: 20 },
+  { nombre: "Sandia", precio: 9 },
 ];
